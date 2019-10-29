439148c5b7c2fca435b3809774a5643393b7af43
Jordan Mercier <jordan.mercier@heig-vd.ch>
2019-10-29 10:45:29 +0100

    Link to delete doesn't work

--- AMT-Project-One/src/main/java/ch/heig/amt/project/one/presentation/RegisterServlet.java
+++ AMT-Project-One/src/main/java/ch/heig/amt/project/one/presentation/RegisterServlet.java
@@ -56,6 +56,7 @@ public class RegisterServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("pathToLogin", Route.LOGIN);
         response.setContentType("text/html;charset=UTF-8");
         request.getRequestDispatcher("/WEB-INF/pages/register.jsp").forward(request, response);
     }
