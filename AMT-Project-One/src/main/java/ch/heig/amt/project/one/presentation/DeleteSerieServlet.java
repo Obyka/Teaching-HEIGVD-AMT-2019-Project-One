72e74325f35b37de65e29309abe0168d4939dca0
Jordan Mercier <jordan.mercier@heig-vd.ch>
2019-11-04 15:56:57 +0100

    Test for servlet DeleteSerieServlet done

--- AMT-Project-One/src/main/java/ch/heig/amt/project/one/presentation/DeleteSerieServlet.java
+++ AMT-Project-One/src/main/java/ch/heig/amt/project/one/presentation/DeleteSerieServlet.java
@@ -9,23 +9,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DeleteSerieServlet extends HttpServlet {
     @EJB
     SeriesManagerLocal seriesManagerLocal;
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        super.doPost(request, response);
-    }
-
-    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String sIdSerie = request.getParameter("idserie");
         User user = (User)request.getSession().getAttribute("user");
-        if(sIdSerie != null) {
-            long idserie = Long.valueOf(sIdSerie);
-            boolean deleted = seriesManagerLocal.delete(user, idserie);
+        if(sIdSerie != null || sIdSerie.trim().equals("")) {
+            try {
+                long idserie = Long.valueOf(sIdSerie);
+                boolean deleted = seriesManagerLocal.delete(user, idserie);
+            } catch (Exception e) {
+                Logger.getLogger(ch.heig.amt.project.one.presentation.DeleteSerieServlet.this.getClass().getName()).log(Level.SEVERE, null, e);
+            }
         }
 
         response.sendRedirect(request.getContextPath() + "/restreint/series");
