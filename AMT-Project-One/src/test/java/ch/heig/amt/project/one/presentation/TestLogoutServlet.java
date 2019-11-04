245fadbbd217693daff77a526c41e62badd3f8cd
Jordan Mercier <jordan.mercier@heig-vd.ch>
2019-11-04 21:17:29 +0100

    Test for servlet LogoutServlet done

---
+++ AMT-Project-One/src/test/java/ch/heig/amt/project/one/presentation/TestLogoutServlet.java
@@ -0,0 +1,44 @@
+package ch.heig.amt.project.one.presentation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class TestLogoutServlet {
+    @Mock
+    HttpServletRequest request;
+    @Mock
+    HttpServletResponse response;
+    @Mock
+    HttpSession session;
+
+    LogoutServlet servlet;
+
+    @BeforeEach
+    public void setup() {
+        servlet = new LogoutServlet();
+
+        when(request.getContextPath()).thenReturn("AMT-project-one");
+        when(request.getSession()).thenReturn(session);
+    }
+
+    @Test
+    void doGet() throws ServletException, IOException {
+        servlet.doGet(request, response);
+
+        verify(request.getSession(), atLeastOnce()).invalidate();
+        verify(response, atLeastOnce()).sendRedirect(request.getContextPath() + "/restreint/login");
+    }
+}
