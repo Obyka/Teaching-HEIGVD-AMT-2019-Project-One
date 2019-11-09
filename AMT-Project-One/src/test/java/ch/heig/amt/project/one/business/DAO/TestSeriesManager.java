e7d160dd028b628e50b9011abc126c885aa7f99a
Jordan Mercier <jordan.mercier@heig-vd.ch>
2019-11-09 22:59:26 +0100

    You can run tests only once, bug about payara fixed

--- AMT-Project-One/src/test/java/ch/heig/amt/project/one/business/DAO/TestSeriesManager.java
+++ AMT-Project-One/src/test/java/ch/heig/amt/project/one/business/DAO/TestSeriesManager.java
@@ -30,6 +30,8 @@ public class TestSeriesManager {
     @EJB
     UsersManagerLocal usersManagerLocal;
 
+    static int SERIE_ID = 0;
+
     @Test
     @Transactional(TransactionMode.ROLLBACK)
     public void itShouldBePossibleToCreateASerie() throws DuplicateKeyException, SQLException {
@@ -40,6 +42,7 @@ public class TestSeriesManager {
         User user = usersManagerLocal.findUserByUsername("Obyka" + currentTime);
         Serie serie = Serie.builder().title("X-Files" + currentTime).producer("Fox").synopsis("Alien thing").ageRestriction(16).genre("Thriller").build();
         serie.setOwner(user.getId());
+        serie.setId(++SERIE_ID);
         boolean serieCreated = seriesManagerLocal.create(serie);
         assertTrue(serieCreated);
     }
@@ -54,10 +57,11 @@ public class TestSeriesManager {
         User user = usersManagerLocal.findUserByUsername("Obyka" + currentTime);
         Serie serie = Serie.builder().title("X-Files" + currentTime).producer("Fox").synopsis("Alien thing").ageRestriction(16).genre("Thriller").build();
         serie.setOwner(user.getId());
+        serie.setId(++SERIE_ID);
         boolean serieCreated = seriesManagerLocal.create(serie);
         assertTrue(serieCreated);
 
-        Serie serieLoaded = seriesManagerLocal.findById(user, 1);
+        Serie serieLoaded = seriesManagerLocal.findById(user, SERIE_ID);
         assertNotSame(serie, serieLoaded);
         assertEquals(serie, serieLoaded);
     }
@@ -72,14 +76,32 @@ public class TestSeriesManager {
         User user = usersManagerLocal.findUserByUsername("Obyka" + currentTime);
         Serie serie = Serie.builder().title("X-Files" + currentTime).producer("Fox").synopsis("Alien thing").ageRestriction(16).genre("Thriller").build();
         serie.setOwner(user.getId());
-        serie.setId(2);
+        serie.setId(++SERIE_ID);
         boolean serieCreated = seriesManagerLocal.create(serie);
         assertTrue(serieCreated);
 
-        serie.setTitle("X-files: Le retour");
+        Serie serieModified = serie;
+        serieModified.setTitle("X-files: Le retour");
 
-        boolean serieUpdated = seriesManagerLocal.update(serie);
+        boolean serieUpdated = seriesManagerLocal.update(serieModified);
         assertTrue(serieUpdated);
     }
 
+    @Test
+    @Transactional(TransactionMode.ROLLBACK)
+    public void itShouldBePossibleToDeleteASerie() throws DuplicateKeyException, SQLException{
+        long currentTime = System.currentTimeMillis();
+        boolean userCreated = usersManagerLocal.create("Obyka" + currentTime, "password");
+        assertTrue(userCreated);
+
+        User user = usersManagerLocal.findUserByUsername("Obyka" + currentTime);
+        Serie serie = Serie.builder().title("X-Files" + currentTime).producer("Fox").synopsis("Alien thing").ageRestriction(16).genre("Thriller").build();
+        serie.setOwner(user.getId());
+        serie.setId(++SERIE_ID);
+        boolean serieCreated = seriesManagerLocal.create(serie);
+        assertTrue(serieCreated);
+
+        boolean serieDeleted = seriesManagerLocal.delete(user, SERIE_ID);
+        assertTrue(serieDeleted);
+    }
 }
