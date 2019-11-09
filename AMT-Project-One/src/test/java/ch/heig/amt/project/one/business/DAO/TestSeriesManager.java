5725adf0dcbb1d3ee4ebda8ad4c3f0f333523c15
Jordan Mercier <jordan.mercier@heig-vd.ch>
2019-11-09 21:11:33 +0100

    First version, not working yet

--- AMT-Project-One/src/test/java/ch/heig/amt/project/one/business/DAO/TestSeriesManager.java
+++ AMT-Project-One/src/test/java/ch/heig/amt/project/one/business/DAO/TestSeriesManager.java
@@ -1,7 +1,9 @@
 package ch.heig.amt.project.one.business.DAO;
 
 import ch.heig.amt.project.one.business.interfaces.SeriesManagerLocal;
+import ch.heig.amt.project.one.business.interfaces.UsersManagerLocal;
 import ch.heig.amt.project.one.model.Serie;
+import ch.heig.amt.project.one.model.User;
 import org.arquillian.container.chameleon.deployment.api.DeploymentParameters;
 import org.arquillian.container.chameleon.deployment.maven.MavenBuild;
 import org.jboss.arquillian.junit.Arquillian;
@@ -15,7 +17,7 @@ import javax.ejb.EJB;
 
 import java.sql.SQLException;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 
 @RunWith(Arquillian.class)
@@ -25,13 +27,59 @@ public class TestSeriesManager {
     @EJB
     SeriesManagerLocal seriesManagerLocal;
 
-    @Test
-    @Transactional(TransactionMode.COMMIT)
-    public void itShouldBePossibleToCreateASerie() throws DuplicateKeyException, SQLException {
-        Serie serie = Serie.builder().title("X-Files" + System.currentTimeMillis()).producer("Fox").synopsis("Alien thing").ageRestriction(16).genre("Thriller").build();
-        serie.setOwner(1);
-        boolean created = seriesManagerLocal.create(serie);
+    @EJB
+    UsersManagerLocal usersManagerLocal;
 
-        assertTrue(created);
+    @Test
+    @Transactional(TransactionMode.ROLLBACK)
+    public void itShouldBePossibleToCreateASerie() throws DuplicateKeyException, SQLException {
+        long currentTime = System.currentTimeMillis();
+        boolean userCreated = usersManagerLocal.create("Obyka" + currentTime, "password");
+        assertTrue(userCreated);
+
+        User user = usersManagerLocal.findUserByUsername("Obyka" + currentTime);
+        Serie serie = Serie.builder().title("X-Files" + currentTime).producer("Fox").synopsis("Alien thing").ageRestriction(16).genre("Thriller").build();
+        serie.setOwner(user.getId());
+        boolean serieCreated = seriesManagerLocal.create(serie);
+        assertTrue(serieCreated);
     }
+
+    @Test
+    @Transactional(TransactionMode.ROLLBACK)
+    public void itShouldBePossibleToFindASerieById() throws DuplicateKeyException, SQLException {
+        long currentTime = System.currentTimeMillis();
+        boolean userCreated = usersManagerLocal.create("Obyka" + currentTime, "password");
+        assertTrue(userCreated);
+
+        User user = usersManagerLocal.findUserByUsername("Obyka" + currentTime);
+        Serie serie = Serie.builder().title("X-Files" + currentTime).producer("Fox").synopsis("Alien thing").ageRestriction(16).genre("Thriller").build();
+        serie.setOwner(user.getId());
+        boolean serieCreated = seriesManagerLocal.create(serie);
+        assertTrue(serieCreated);
+
+        Serie serieLoaded = seriesManagerLocal.findById(user, 1);
+        assertNotSame(serie, serieLoaded);
+        assertEquals(serie, serieLoaded);
+    }
+
+    @Test
+    @Transactional(TransactionMode.ROLLBACK)
+    public void itShouldBePossibleToUpdateASerie() throws DuplicateKeyException, SQLException {
+        long currentTime = System.currentTimeMillis();
+        boolean userCreated = usersManagerLocal.create("Obyka" + currentTime, "password");
+        assertTrue(userCreated);
+
+        User user = usersManagerLocal.findUserByUsername("Obyka" + currentTime);
+        Serie serie = Serie.builder().title("X-Files" + currentTime).producer("Fox").synopsis("Alien thing").ageRestriction(16).genre("Thriller").build();
+        serie.setOwner(user.getId());
+        serie.setId(2);
+        boolean serieCreated = seriesManagerLocal.create(serie);
+        assertTrue(serieCreated);
+
+        serie.setTitle("X-files: Le retour");
+
+        boolean serieUpdated = seriesManagerLocal.update(serie);
+        assertTrue(serieUpdated);
+    }
+
 }
