7881547ba7ff64d363876a064dc6019e23de5946
Jordan Mercier <mercier.jo98@gmail.com>
2019-11-10 16:26:30 +0100

    Change script to import ssl certificate

--- AMT-Project-One/ssl/import.sh
+++ AMT-Project-One/ssl/import.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
- #sudo keytool -delete -alias payara -keystore /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security/cacerts ## Remove the certificate
-sudo keytool -trustcacerts -keystore /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security/cacerts -storepass changeit -alias payara -import -file /home/jordan/Documents/AMT/Labos/Project-One/Teaching-HEIGVD-AMT-2019-Project-One/AMT-Project-One/ssl/payara-self-signed-certificate.crt
-keystore
+#sudo keytool -delete -alias payara -keystore /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security/cacerts ## Remove the certificate
+sudo keytool -trustcacerts -keystore /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security/cacerts -storepass changeit -alias payara -import -file ./payara-self-signed-certificate.crt
