026bb94c9085100a379724e9678930de4f191ce0
Florian Polier <polier.florian@gmail.com>
2019-11-11 19:18:42 +0100

    modified scripts

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
+docker-compose -f AMT-Project-One/docker/topology-project-one/docker-compose.yml down
 cd AMT-Project-One
 mvn clean install -DskipTests
 cd ..
 cp ./AMT-Project-One/target/AMT-project-one.war images/payara/
-cd topology-project-one
-docker-compose down
-docker-compose up --build
+docker-compose -f topology-project-one/docker-compose.yml down
+docker-compose -f topology-project-one/docker-compose.yml up --build
---
+++ test.sh
@@ -0,0 +1,4 @@
+docker-compose -f topology-project-one/docker-compose.yml down
+docker-compose -f AMT-Project-One/docker/topology-project-one/docker-compose.yml down
+docker-compose -f AMT-Project-One/docker/topology-project-one/docker-compose.yml up --build
+
